71539a1e375549aa5a3aac2d84cea30b33582201
Giuseppe Lo Basso <giuseppe.lobasso@outlook.it>
2026-06-04 01:56:47 +0200

    v2

---
+++ resources/views/layout/v2.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Firefly III</title>
+    <script nonce="{{ $JS_NONCE }}">window.__localeId__ = '{{ str_replace('-', '_', app()->getLocale()) }}';</script>
+    @vite(['resources/assets/v2/src/css/app.css', 'resources/assets/v2/src/main.js'], 'build/v2')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
